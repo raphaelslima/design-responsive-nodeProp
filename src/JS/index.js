492f3833f2b044fd7a98721b2e2a7209283d5d1a
Raphael Lima <raphaeldesousalm@gmail.com>
2023-03-10 16:12:34 -0300

    Finalizado

---
+++ src/JS/index.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menu");
+
+function toggle() {
+  if (menu.style.display != "none") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
